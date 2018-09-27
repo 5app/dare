5dfa405494d97d038520ef4b8b4ede8d82855391
Andrew Dodson <andrew@5app.com>
2018-09-27 17:31:56 +0100

    fix: allowing ? in a field label is incompatible with mysql.format

--- src/utils/validate_label.js
+++ src/utils/validate_label.js
@@ -1,7 +1,7 @@
 const DareError = require('./error');
 
 module.exports = function validate_label(label) {
-	const reg = /^[^'"]+$/i;
+	const reg = /^[^'"?`]+$/i;
 
 	// Capture errors in the key
 	if (!label.match(reg)) {
