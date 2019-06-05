ab664683fce68de76770ba18f8aa92931b7f0b47
Andrew Dodson <andrew@5app.com>
2019-06-05 13:13:42 +0100

    style(jsdoc): enforce jsdoc syntax rules

--- src/utils/promisify.js
+++ src/utils/promisify.js
@@ -2,8 +2,8 @@
  * Promisify a function if it's not already a Promise.
  * This works differently to native util.promisify because it will accept a function which is already wrapped in a promise
  *
- * @params function to wrap into a promise
- * @returns the function promisified, so can be called with parameters as it was before but without the CommonJS callback
+ * @param {Function} func - The function to wrap into a promise
+ * @returns {Function} the function promisified, so can be called with parameters as it was before but without the CommonJS callback
  */
 module.exports = function promisify(func) {
 
