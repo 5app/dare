18e65204986f6ef38c69b90ec7f58c805c876931
Andrew Dodson <andrew@5app.com>
2019-10-18 16:12:13 +0100

    test(style): tidy, noissue

--- test/setup.js
+++ test/setup.js
@@ -2,12 +2,12 @@
 
 const Dare = require('../src/');
 const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+// Extend Chai
+chai.use(chaiAsPromised);
 
 // Set Globals
 global.Dare = Dare;
 
 global.expect = chai.expect;
-
-// Global.sinon = require('sinon');
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
