35bc8decec437593325d72285d7e1fb590575b0c
Andrew Dodson <andrew@5app.com>
2023-02-17 08:35:46 +0000

    fix(patch/del): nested table handlers, fixes #277

--- test/integration/multi-table-modifications.spec.js
+++ test/integration/multi-table-modifications.spec.js
@@ -126,6 +126,85 @@ describe('Multi table modifications', () => {
 			);
 		}
 	});
+
+	it('patch operations should use the get handler for nested model conditions', async () => {
+		const code = 'UK';
+
+		/*
+		 * Setup
+		 * Create a user in each country
+		 */
+		const countries = await dare.get('country', ['id'], {}, {limit: 100});
+
+		const users = await dare.post(
+			'users',
+			['test1', 'test2'].map((username, index) => ({
+				username,
+				country_id: countries[index].id,
+			}))
+		);
+
+		const user_ids = sequence(users);
+
+		{
+			// Pre-test, ensure model change is
+			const resp = await dare.patch({
+				table: 'users',
+				body: {
+					first_name: 'Old name',
+				},
+				filter: {
+					id: user_ids,
+				},
+				limit: 100,
+			});
+
+			assert.strictEqual(
+				resp.affectedRows,
+				2,
+				'Should affect both users'
+			);
+		}
+
+		// Extend dare model with the handlers
+		dare = dare.use({
+			models: {
+				users: {
+					patch(options) {
+						// Ensure this joins to country
+						options.join = {
+							country: {
+								// Required will ensure it's added
+								_required: true,
+							},
+						};
+					},
+				},
+				country: {
+					get(options) {
+						options.filter = {code};
+					},
+					patch() {
+						throw Error('teams can not be mutated');
+					},
+				},
+			},
+		});
+
+		// Try updating all the users...
+		const resp = await dare.patch({
+			table: 'users',
+			body: {
+				first_name: 'New name',
+			},
+			filter: {
+				id: user_ids,
+			},
+			limit: 100,
+		});
+
+		assert.strictEqual(resp.affectedRows, 1, 'Should only affect 1 row');
+	});
 });
 
 /**
