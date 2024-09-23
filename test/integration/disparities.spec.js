00b8c54385b79cea1de943ea3682dd702a361494
Andrew Dodson <andrew@5app.com>
2024-09-23 15:49:08 +0100

    test(postgres): fix breaking test for postgres, noissue

--- test/integration/disparities.spec.js
+++ test/integration/disparities.spec.js
@@ -4,6 +4,7 @@ import assert from 'node:assert/strict';
 import mysql from 'mysql2/promise';
 import db from './helpers/db.js';
 import {options} from './helpers/api.js';
+import SQL from 'sql-template-tag';
 const debug = Debug('sql');
 
 // Connect to db
@@ -26,60 +27,60 @@ describe(`Disparities`, () => {
 	});
 
 	it('MySQL 8 fails to correctly count the items in this scenario', async () => {
-		// Add an additional column to the pivot table
-		await dare.sql(`
+		/*
+		 * See Bug report: https://bugs.mysql.com/bug.php?id=109585
+		 */
+		await dare.sql(SQL`
 
-			CREATE TABLE \`members\` (
-				\`id\` int NOT NULL AUTO_INCREMENT,
-				\`name\` VARCHAR(20) NOT NULL,
-				PRIMARY KEY (\`id\`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+			CREATE TABLE members (
+				id int NOT NULL,
+				name VARCHAR(30) NOT NULL,
+				PRIMARY KEY (id)
+			);
 
-			CREATE TABLE \`content\` (
-				\`id\` int NOT NULL AUTO_INCREMENT,
-				\`name\` VARCHAR(20) NOT NULL,
-				PRIMARY KEY (\`id\`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+			CREATE TABLE content (
+				id int NOT NULL,
+				name VARCHAR(30) NOT NULL,
+				PRIMARY KEY (id)
+			);
 			
-			CREATE TABLE \`domains\` (
-				\`id\` smallint unsigned NOT NULL AUTO_INCREMENT,
-				\`name\` VARCHAR(100) DEFAULT NULL,
-				PRIMARY KEY (\`id\`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+			CREATE TABLE domains (
+				id smallint NOT NULL,
+				name VARCHAR(100) NULL,
+				PRIMARY KEY (id)
+			);
 
-			CREATE TABLE \`userContent\` (
-				\`id\` int NOT NULL AUTO_INCREMENT,
-				\`content_id\` int NOT NULL,
-				\`user_id\` int NOT NULL,
-				\`domain_id\` smallint unsigned NOT NULL,
-				\`status\` enum('notstarted','opened','completed') NOT NULL DEFAULT 'notstarted',
-				PRIMARY KEY (\`id\`),
-				UNIQUE KEY \`content_user_domain_id\` (\`content_id\`,\`user_id\`,\`domain_id\`),
-				KEY \`userContent_user\` (\`user_id\`),
-				KEY \`fk_userContent_domain_id\` (\`domain_id\`),
-				CONSTRAINT \`fk_userContent_domain_id\` FOREIGN KEY (\`domain_id\`) REFERENCES \`domains\` (\`id\`),
-				CONSTRAINT \`userContent_content\` FOREIGN KEY (\`content_id\`) REFERENCES \`content\` (\`id\`) ON DELETE CASCADE,
-				CONSTRAINT \`userContent_user\` FOREIGN KEY (\`user_id\`) REFERENCES \`members\` (\`id\`) ON DELETE CASCADE
-			  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+			CREATE TABLE userContent (
+				id int NOT NULL,
+				content_id int NOT NULL,
+				user_id int NOT NULL,
+				domain_id smallint NOT NULL,
+				status VARCHAR(20) NOT NULL DEFAULT 'notstarted',
+				PRIMARY KEY (id),
+				CONSTRAINT content_user_domain_id UNIQUE (content_id,user_id,domain_id),
+				CONSTRAINT fk_userContent_domain_id FOREIGN KEY (domain_id) REFERENCES domains (id),
+				CONSTRAINT userContent_content FOREIGN KEY (content_id) REFERENCES content (id) ON DELETE CASCADE,
+				CONSTRAINT userContent_user FOREIGN KEY (user_id) REFERENCES members (id) ON DELETE CASCADE
+			  );
 			  
 			  -- ---------------------------------
 			  -- INSERT
 			  -- ---------------------------------
 			  
-			  INSERT INTO domains (id, \`name\`) VALUES (1, 'Test');
+			  INSERT INTO domains (id, name) VALUES (1, 'Test');
 			  
-			  INSERT INTO content (id, \`name\`) VALUES (1, 'A'), (2, 'B'), (3, 'C');
+			  INSERT INTO content (id, name) VALUES (1, 'A'), (2, 'B'), (3, 'C');
 			  
-			  INSERT INTO members (\`id\`,\`name\`)
+			  INSERT INTO members (id,name)
 			  VALUES
 				  (11, 'devuser11@example.com'),
 				  (12, 'devuser12@example.com');
 			  
-			  INSERT INTO userContent (\`domain_id\`,\`content_id\`,\`user_id\`,\`status\`)
+			  INSERT INTO userContent (id, domain_id,content_id,user_id,status)
 			  VALUES
-				  (1, 3, 11, 'completed'),
-				  (1, 3, 12, 'completed'),
-				  (1, 2, 12, 'completed');
+				  (1, 1, 3, 11, 'completed'),
+				  (2, 1, 3, 12, 'completed'),
+				  (3, 1, 2, 12, 'completed');
 
 		`);
 
